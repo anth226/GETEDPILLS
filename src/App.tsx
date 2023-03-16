80adc0a26ca011fec43ea4144828cbac61b1bbce
wasiq abdullah <chwasiq0569>
2023-03-16 11:58:11 +0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FAQs from './Pages/FAQs';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <FAQs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/FAQs.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const FAQs: React.FC = () => {
+    return (<div> <h1>FAQ</h1></div>);
+}
+
+export default FAQs;
